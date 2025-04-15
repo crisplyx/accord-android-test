354530c3a71c034838e523a3f4b3a7c15f7db7f8
ZeroSource <crisplyx@gmail.com>
2025-04-15 18:42:55 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.dagger.hilt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
+    id("com.google.devtools.ksp") version "2.1.20-2.0.0" apply false
 }
